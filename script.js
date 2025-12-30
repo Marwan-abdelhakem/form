5383c95777fe697b708c26b9f60875c5f05e62ab
Marwan abdelhakem <marwanabdelhakem@gmail.com>
2025-12-30 17:20:13 +0200

    js

--- script.js
+++ script.js
@@ -1,41 +1,3 @@
-
-// const form = document.getElementById("userform")
-// const table = document.getElementById("userTable");
-
-
-// form.addEventListener("submit", function (ev) {
-//     ev.preventDefault()
-
-//     const userDate = {
-//         name: document.getElementById("name").value,
-//         phone: document.getElementById("phone").value,
-//         address: document.getElementById("address").value,
-//         date: document.getElementById("date").value,
-//         job: document.getElementById("job").value
-//     }
-//     table.style.display = "table";   ///1
-
-//     const row = table.insertRow(); 
-//     row.insertCell(0).innerText = userDate.name;
-//     row.insertCell(1).innerText = userDate.phone;
-//     row.insertCell(2).innerText = userDate.address;
-//     row.insertCell(3).innerText = userDate.date;
-//     row.insertCell(4).innerText = userDate.job;
-
-//     const deleteCell = row.insertCell(5);
-//     deleteCell.innerHTML = `<button class="delete-btn">Delete</button>`;
-
-//     deleteCell.querySelector(".delete-btn").addEventListener("click", function () {
-//         table.deleteRow(row.rowIndex);
-//         if (table.rows.length === 1) {
-//             table.style.display = "none";
-//         }
-//     });
-//     form.reset();
-// })
-
-// da el part after updated
-
 const form = document.getElementById("userform");
 const table = document.getElementById("userTable");
 const tbody = table.querySelector("tbody");
@@ -74,25 +36,6 @@ table.addEventListener("click", function (e) {
         if (tbody.children.length === 0) {
             table.classList.add("hidden");
         }
-
-        table.style.display
-
-        const row = table.insertRow();
-        row.insertCell(0).innerText = userDate.name;
-        row.insertCell(1).innerText = userDate.phone;
-        row.insertCell(2).innerText = userDate.address;
-        row.insertCell(3).innerText = userDate.date;
-        row.insertCell(4).innerText = userDate.job;
-
-        const deleteCell = row.insertCell(5);
-        deleteCell.innerHTML = `<button class="delete-btn">Delete</button>`;
-
-        deleteCell.querySelector(".delete-btn").addEventListener("click", function () {
-            table.deleteRow(row.rowIndex);
-            if (table.rows.length === 1) {
-                table.style.display = "none";
-            }
-        });
-        form.reset();
-    })
+    }
+});
 
